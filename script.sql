2a32b46ba4b4544dc92c47ea63e559c5fb93b08f
abderrazzak imily <imily2024@gmail.com>
2024-12-11 18:13:47 +0100

    add sql script

--- script.sql
+++ script.sql
@@ -1,3 +1,8 @@
+
+CREATE DATABASE sallegym;
+
+USE sallegym;
+
 CREATE TABLE membres (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(50),
@@ -16,6 +21,10 @@ CREATE TABLE reservations (
     membre_id INT,
     activite_id INT,
     date_reservation DATE,
-    FOREIGN KEY (membre_id) REFERENCES membres(id),
-    FOREIGN KEY (activite_id) REFERENCES activites(id)
+    FOREIGN KEY (membre_id) REFERENCES membres(id) ON DELETE CASCADE  ON UPDATE CASCADE,
+    FOREIGN KEY (activite_id) REFERENCES activites(id) ON DELETE CASCADE  ON UPDATE CASCADE,
 );
+
+
+INSERT INTO activites(nom , description)
+VALUES ('yoga' , 'A yoga practice is a lifelong pursuit, giving you plenty of time to explore each asana (pose) and learn sequences of postures') , ('cardio' , 'Le cardio-training est une activité sportive qui sollicite le système cardio-vasculaire');
